cc832a62b89643e9a27b52336e98d853d08d0087
JJ <tangjun@zuzuche.com>
2018-11-09 18:45:05 +0800

    remove test

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,8 +2,6 @@
 module.exports = {
   async index (ctx) {
     let res = await ctx.service.main.getName();
-    let data = await ctx.db.user.find({});
-    console.log(data);
     ctx.body = ctx.nunjucks.render('index.html', res.data);
   }
 }
